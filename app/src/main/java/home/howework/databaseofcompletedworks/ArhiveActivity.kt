597a18bc8d7719300c31f8ade1a543bb649de5e0
Тимур <novogiryevotm@mail.ru>
2024-06-07 14:04:51 +0300

    Добавление отправки отчёта в Excel формате

--- app/src/main/java/home/howework/databaseofcompletedworks/ArhiveActivity.kt
+++ app/src/main/java/home/howework/databaseofcompletedworks/ArhiveActivity.kt
@@ -1,5 +1,6 @@
 package home.howework.databaseofcompletedworks
 
+
 import android.annotation.SuppressLint
 import android.content.DialogInterface
 import android.content.Intent
@@ -10,33 +11,31 @@ import android.text.format.DateFormat
 import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import home.howework.databaseofcompletedworks.ArhiveActivity.CabInst.cabinetIn
-import home.howework.databaseofcompletedworks.ArhiveActivity.CabInst.fotoUri
 import home.howework.databaseofcompletedworks.databinding.ArhiveActivityBinding
 import home.howework.databaseofcompletedworks.databinding.ListItemCabinetArhiveBinding
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.hssf.util.HSSFColor
+import org.apache.poi.ss.usermodel.IndexedColors
 import java.io.File
-import java.util.UUID
+import java.io.FileOutputStream
+import java.util.Date
+
 
 class ArhiveActivity : AppCompatActivity() {
     private lateinit var photoFile: File
@@ -50,9 +49,13 @@ class ArhiveActivity : AppCompatActivity() {
     var density = 0.0f
     lateinit var filesDir2: File
     private var adapter: ArhiveAdapter? = ArhiveAdapter(emptyList()) { }
+    private val arhiveRepository = SharedRepo()
     private val cabinetListViewModel: CabinetListViewModel by viewModels()
+    var excelCabinets: List<Cabinet> = emptyList<Cabinet>()
 
     companion object {
+        const val DEFAULT_TEMP_DIR_NAME = "CABINET"
+        var DEFAULT_TEMP_FILENAME = ""
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,10 +68,14 @@ class ArhiveActivity : AppCompatActivity() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 cabinetListViewModel.cabinets.collect { cabinets ->
+                    excelCabinets = cabinets
                     binding.arhiveRecyclerView.adapter =
                         ArhiveAdapter(cabinets) { cab -> deleteProcess(cab) }
                 }
             }
+        }
+        binding.sendExcelFile.setOnClickListener {
+             sendExcelList(excelCabinets)
         }
         when (resources.displayMetrics.densityDpi) {
             DisplayMetrics.DENSITY_LOW -> {
@@ -134,7 +141,11 @@ class ArhiveActivity : AppCompatActivity() {
     }
 
     fun toast(coefficient: Float) {
-        Toast.makeText(this@ArhiveActivity, "плотность экрана телефона:${coefficient}", Toast.LENGTH_SHORT)
+        Toast.makeText(
+            this@ArhiveActivity,
+            "плотность экрана телефона:${coefficient}",
+            Toast.LENGTH_SHORT
+        )
             .show()
     }
 
@@ -143,13 +154,13 @@ class ArhiveActivity : AppCompatActivity() {
         val inflater = getLayoutInflater();
         val layout = inflater.inflate(R.layout.toast_layout, findViewById(R.id.toast_layout_root));
         val image = layout.findViewById<ImageView>(R.id.image_toast);
-        val text =layout.findViewById<TextView>(R.id.toast_text);
-       // text.text = "Удаление ${cabinet.title} время сборки: ${cabinet.date}";
+        val text = layout.findViewById<TextView>(R.id.toast_text);
+        // text.text = "Удаление ${cabinet.title} время сборки: ${cabinet.date}";
         text.text = "${cabinet.title} удалён!";
         val toast = Toast(applicationContext);
         toast.setGravity(Gravity.CENTER_VERTICAL, 60, 260);
         toast.duration = Toast.LENGTH_LONG;
-        if(photoFile.path!=null&&photoFile.exists()) {
+        if (photoFile.path != null && photoFile.exists()) {
             val scaledBitmap = getScaledBitmap(
                 photoFile.path,
                 130,
@@ -162,24 +173,33 @@ class ArhiveActivity : AppCompatActivity() {
     }
 
     fun deleteProcess(cabinet: Cabinet) {
-        cabinetIn = cabinet
-        val closeWarnings = AlertDialog.Builder(this)
-        closeWarnings.setTitle("Вы действительно хотите удалить кабинет?")
-        closeWarnings.setMessage("Будут удалены  фотографии и записи кабинета! ")
-        closeWarnings.setIcon(R.drawable.reset)
-        closeWarnings.setNegativeButton("Отмена", object : DialogInterface.OnClickListener {
-            override fun onClick(p0: DialogInterface?, p1: Int) {
-
-            }
-        })
-        closeWarnings.setCancelable(false)
-        closeWarnings.setPositiveButton("Удалить",
-            object : DialogInterface.OnClickListener {
+        if (!arhiveRepository.getResumeDayState(this)) {
+            cabinetIn = cabinet
+            val closeWarnings = AlertDialog.Builder(this)
+            closeWarnings.setTitle("Вы действительно хотите удалить кабинет?")
+            closeWarnings.setMessage("Будут удалены  фотографии и записи кабинета! ")
+            closeWarnings.setIcon(R.drawable.reset)
+            closeWarnings.setNegativeButton("Отмена", object : DialogInterface.OnClickListener {
                 override fun onClick(p0: DialogInterface?, p1: Int) {
-                    deleteProcessImpl(cabinetIn)
+
                 }
             })
-        closeWarnings.show()
+            closeWarnings.setCancelable(false)
+            closeWarnings.setPositiveButton("Удалить",
+                object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        deleteProcessImpl(cabinetIn)
+                    }
+                })
+            closeWarnings.show()
+        } else {
+            Toast.makeText(
+                this@ArhiveActivity,
+                "Нельзя удалять кабинеты в режиме Пауза!",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
     }
 
     fun deleteProcessImpl(cabinet: Cabinet) {
@@ -254,6 +274,94 @@ class ArhiveActivity : AppCompatActivity() {
 
     }
 
+    private fun sendExcelList(cabinets: List<Cabinet>) {
+        if (cabinets.isNotEmpty()) {
+            val date: Date = Date()
+            val DATE_FORMAT = "dd MM yyyy HH:mm:ss"
+            val DATE_FORMAT2 = "dd MM yyyy"
+            DEFAULT_TEMP_FILENAME = "/" + arhiveRepository.getName(this@ArhiveActivity)
+                .toString() + "_" + DateFormat.format(DATE_FORMAT, date) + ".xls"
+            //val imgPath: File = File(this.cacheDir, DEFAULT_TEMP_DIR_NAME)
+            val newFile = File(filesDir2, DEFAULT_TEMP_FILENAME)
+            val uriExcelFile = FileProvider.getUriForFile(
+                this@ArhiveActivity,
+                "home.howework.databaseofcompletedworks.fileprovider",
+                newFile
+            )
+            val hssfWorkbook = HSSFWorkbook()
+            val hssfSheet =
+                hssfWorkbook.createSheet("Лист отчёта ${arhiveRepository.getName(this@ArhiveActivity)}а")
+//            val styleCell = hssfWorkbook.createCellStyle()
+//            styleCell.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+            val size = cabinets.size
+            val hssfRow = hssfSheet.createRow(4)
+            val hssfCell = hssfRow.createCell(5)
+            hssfCell.setCellValue(" Итоговая сводка из архива собранных кабинетов ")
+            var n = 0
+            repeat(size) {
+                val hssfRowIn = hssfSheet.createRow(6 + n)
+                val hssfCellIn = hssfRowIn.createCell(4)
+                if (cabinets[n].isMainCabinet) {
+                    val styleCell = hssfWorkbook.getCellStyleAt(1)
+                    //     styleCell.fillBackgroundColor = HSSFColor.GREEN.index
+
+                    //   hssfCellIn.setCellStyle(styleCell)
+                    //  val cell= hssfRowIn.getCell(4)
+                    // hssfCellIn.setCellValue(cabinets[n].title)
+                    //   cell.setCellStyle(styleCell)
+                    hssfCellIn.setCellValue(cabinets[n].title)
+                    // hssfCellIn.hyperlink
+                } else {
+                    hssfCellIn.setCellValue(cabinets[n].title)
+                }
+                val hssfCellInDate = hssfRowIn.createCell(5)
+                hssfCellInDate.setCellValue("${DateFormat.format(DATE_FORMAT2, cabinets[n].date)}")
+                n += 1
+            }
+
+
+            try {
+                if (!newFile.exists()) {
+                    newFile.createNewFile()
+                }
+                val fileOutputStream: FileOutputStream = FileOutputStream(newFile)
+                hssfWorkbook.write(fileOutputStream)
+                if (fileOutputStream != null) {
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                }
+                if (uriExcelFile != null) {
+                    val sendIntent = Intent()
+                    sendIntent.action = Intent.ACTION_SEND
+                    sendIntent.putExtra(
+                        Intent.EXTRA_STREAM,
+                        uriExcelFile
+                    )
+                    sendIntent.setType("text/html")
+                    sendIntent.setPackage("com.whatsapp")
+                    startActivity(sendIntent)
+//                val shareIntent = ShareCompat.IntentBuilder.from(this@ArhiveActivity)
+//                    .setType("text/html")
+//                    .setStream(uriExcelFile)
+//                    .setSubject("Отправка книги Excel")
+//                    .setEmailTo(arrayOf<String>("example@example.com"))
+//                    .intent
+//                shareIntent.data = uriExcelFile
+//                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                this@ArhiveActivity.startActivity(
+//                    Intent.createChooser(
+//                        shareIntent, "Передача контента"
+//                    )
+//                )
+                }
+
+            } catch (e: Exception) {
+
+            }
+
+        }
+    }
+
     override fun onBackPressed() {
         val count = supportFragmentManager.backStackEntryCount
         if (count == 0) {
@@ -325,7 +433,7 @@ class ArhiveActivity : AppCompatActivity() {
 
     object CabInst {
         lateinit var cabinetIn: Cabinet
-       var fotoUri: Uri?=null
+        var fotoUri: Uri? = null
     }
 
 }
